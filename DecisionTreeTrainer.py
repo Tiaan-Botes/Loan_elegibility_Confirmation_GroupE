8b843d8d3448ee1343dbd129ffb5b707084fa917
Sean Botha <seanbotha2003@gmail.com>
2024-05-08 10:22:13 +0200

    Removed old files

--- DecisionTreeTrainer.py
+++ DecisionTreeTrainer.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score
 import plotly.express as px
 
 # Prep Loan.csv
-df = pd.read_csv('C:\Users\Zegert\Documents\GitHub\Loan_elegibility_Confirmation_GroupE\Loans.csv')
+df = pd.read_csv('C:\\Users\\seanb\\OneDrive\\Documents\\GitHub\\Loan_elegibility_Confirmation_GroupE\\Loans updated.csv')
 
 irrelevant_features = ['Loan_ID']
 df.drop(columns=irrelevant_features, inplace=True)
@@ -47,39 +47,3 @@ plt.figure(figsize=(18, 12))
 plot_tree(decision_tree=final_model_dt, filled=True, max_depth=2, feature_names=feature_names, class_names=True)
 plt.axis('off')
 plt.show()
-'''
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import metrics
-
-def train_decision_tree(C:\Users\Zegert\Documents\GitHub\Loan_elegibility_Confirmation_GroupE\Loans.csv):
-    df = pd.read_csv(file_path)
-    X = df.drop('Loan_Status', axis=1)
-    y = df['Loan_Status']
-
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-    clf = DecisionTreeClassifier(random_state=1)
-    clf.fit(X_train, y_train)
-
-    # Predict the loan status for the test data
-    y_pred = clf.predict(X_test)
-
-    print("DEcision Tree Accuracy:", metrics.accuracy_score(y_test, y_pred))
-
-    fig = plt.figure(figsize=(15,10))
-    _ = tree.plot_tree(clf, 
-                       feature_names=X.columns,  
-                       class_names=['Loan Approved', 'Loan Not Approved'],
-                       filled=True)
-
-    corr = df.corr()
-    sns.heatmap(corr, annot=True, cmap='coolwarm')
-    plt.show()
-
-train_decision_tree(r'C:\Users\Zegert\Documents\GitHub\Loan_elegibility_Confirmation_GroupE\Loans.csv')
-'''
